c43d4641584453581b2a2605276ab3618c7fb838
Cassandra de Git <cassandra.m.degit@gmail.com>
2015-03-27 02:14:05 -0700

    Change KML Layer based on selected view

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+var views = {
+    crimeOverTime: {
+        map: {
+            kmlLayer: {},
+            callback: function () {}
+        },
+        chart: {
+            data: {},
+            callback: function() {}
+        }
+    },
+    parking: {
+        map: {
+            kmlLayer: {},
+            callback: function () {}
+        },
+        chart: {
+            data: {},
+            callback: function() {}
+        }
+    },
+    trafficCalming: {
+        map: {
+            kmlLayer: {},
+            callback: function () {}
+        },
+        chart: {
+            data: {},
+            callback: function() {}
+        }
+    },
+};
+
 var mapProperties = {
     center:new google.maps.LatLng(49.1833, -122.85),
     zoom:11,
@@ -5,31 +38,30 @@ var mapProperties = {
 };
 var map = new google.maps.Map(document.getElementById("gMap"), mapProperties);
 
-function drawMap() {
-    // var marker=new google.maps.Marker({
-    //     position: mapProperties.center,
-    // });
-
-    // marker.setMap(map);
-
-    var goldStar = {
-        path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-        fillColor: 'yellow',
-        fillOpacity: 0.8,
-        scale: 1,
-        strokeColor: 'gold',
-        strokeWeight: 14
-    };
-
-
-    var ctaLayer = new google.maps.KmlLayer(
+function initKMLLayers() {
+    views.crimeOverTime.map.kmlLayer = new google.maps.KmlLayer(
     {   
         url: 'https://raw.githubusercontent.com/cdegit/iat355-surrey-crime/master/data/crime2013.kml?token=ACubUusFqD6-hhwSkiukcGskxzP9qHoXks5VHkTlwA%3D%3D',
         preserveViewport: true,
-        icon: goldStar
+        map: null
     });
-    ctaLayer.setMap(map);
 
+    views.parking.map.kmlLayer = new google.maps.KmlLayer(
+    {   
+        url: 'https://raw.githubusercontent.com/cdegit/iat355-surrey-crime/master/data/parking.kml?token=ACubUifc8Rhjl8oVQvDf1ofQ9hP1pBKXks5VHlaMwA%3D%3D',
+        preserveViewport: true,
+        map: null
+    });
+
+    views.trafficCalming.map.kmlLayer = new google.maps.KmlLayer(
+    {   
+        url: 'https://raw.githubusercontent.com/cdegit/iat355-surrey-crime/master/data/traffic_calming.kml?token=ACubUv1XPeOOPBAf0X4RC-pzc4Ki21xlks5VHlaVwA%3D%3D',
+        preserveViewport: true,
+        map: null
+    });
+}
+
+function drawMap() {
     var styles = [
     {
         stylers: [
@@ -52,8 +84,11 @@ function drawMap() {
     }];
 
     map.setOptions({styles: styles});
-
 }
+
+initKMLLayers();
+
+views.crimeOverTime.map.kmlLayer.setMap(map);
 
 $(drawMap)
 
@@ -82,3 +117,12 @@ function resizeVis(mapSize, chartSize) {
         map.setCenter(center); 
     }, 300);
 }
+
+d3.select("select").on("change", function() {
+    for (view in views) {
+        views[view].map.kmlLayer.setMap(null);
+    }
+
+    var select = d3.select(d3.event.target);
+    views[select.property("value")].map.kmlLayer.setMap(map);
+});
