1ebc091fd4b0f6ff063d920d541193b1a9962789
Cassandra de Git <cassandra.m.degit@gmail.com>
2015-04-06 16:57:11 -0700

    Changed data points for crimes from 200 to 500

--- geocoding_json.js
+++ geocoding_json.js
@@ -12,60 +12,73 @@ var geocodedAddresses = [];
 
 var geoIndex = 0;
 
-fs.readFile("data/random_crime2014.csv", "utf8", function(err, data) {
-	data = data.split("\n");
+var numberToGeocode = 500;
 
-	data.forEach(function(line, index, arr) {
-		if (line !== "" && index !== 0) {
-			var splitLine = line.split(",");
+if (process.argv[2]) {
+	if (process.argv[2] == "2013") {
+		var newFile = "data/crime2013.json";
+		convert2013data();
+	} else if (process.argv[2] == "2014") {
+		var newFile = "data/crime2014.json"; 
+		convert2014data();
+	}
+}
 
-			if (splitLine[1] !== "") {
+function convert2014data() {
+	fs.readFile("data/random_crime2014.csv", "utf8", function(err, data) {
+		data = data.split("\n");
+
+		data.forEach(function(line, index, arr) {
+			if (line !== "" && index !== 0) {
+				var splitLine = line.split(",");
+
+				if (splitLine[1] !== "") {
+					addresses.push({
+						addr: splitLine[1] + " " + splitLine[2].trim() + ", Surrey, BC",
+						type: splitLine[0],
+						month: splitLine[3],
+						year: splitLine[4]
+					});
+				} else {
+					addresses.push({
+						addr: splitLine[2].split(" /").join().trim() + ", Surrey, BC",
+						type: splitLine[0],
+						month: splitLine[3],
+						year: splitLine[4]
+					});
+				}
+			}
+		});
+
+		addresses = addresses.slice(0, numberToGeocode)
+		console.log(addresses)
+
+		geocoder.geocode(addresses[geoIndex].addr, fetchNextAddress);
+	});
+}
+
+function convert2013data() {
+	fs.readFile("data/random_crime2013.csv", "utf8", function(err, data) {
+		data = data.split("\n");
+
+		data.forEach(function(line, index, arr) {
+			if (line !== "" && index !== 0) {
+				var splitLine = line.split(",");
 				addresses.push({
-					addr: splitLine[1] + " " + splitLine[2].trim() + ", Surrey, BC",
+					addr: splitLine[1] + ", Surrey, BC",
 					type: splitLine[0],
-					month: splitLine[3],
-					year: splitLine[4]
-				});
-			} else {
-				// ugh
-				//addresses.push(splitLine[2].split(" /").join().trim() + ", Surrey, BC");
-
-				addresses.push({
-					addr: splitLine[2].split(" /").join().trim() + ", Surrey, BC",
-					type: splitLine[0],
-					month: splitLine[3],
-					year: splitLine[4]
+					month: splitLine[2],
+					year: splitLine[3]
 				});
 			}
-		}
+		});
+
+		addresses = addresses.slice(0, numberToGeocode)
+		console.log(addresses)
+
+		geocoder.geocode(addresses[geoIndex].addr, fetchNextAddress);
 	});
-
-	addresses = addresses.slice(0, 200)
-	console.log(addresses)
-
-	geocoder.geocode(addresses[geoIndex].addr, fetchNextAddress);
-});
-
-// fs.readFile("data/random_crime2013.csv", "utf8", function(err, data) {
-// 	data = data.split("\n");
-
-// 	data.forEach(function(line, index, arr) {
-// 		if (line !== "" && index !== 0) {
-// 			var splitLine = line.split(",");
-// 			addresses.push({
-// 				addr: splitLine[1] + ", Surrey, BC",
-// 				type: splitLine[0],
-// 				month: splitLine[2],
-// 				year: splitLine[3]
-// 			});
-// 		}
-// 	});
-
-// 	addresses = addresses.slice(0, 200)
-// 	console.log(addresses)
-
-// 	geocoder.geocode(addresses[geoIndex].addr, fetchNextAddress);
-// });
+}
 
 function fetchNextAddress(err, res) {
 	if (err) {
@@ -99,7 +112,7 @@ function fetchNextAddress(err, res) {
 };
 
 function writeJson() {
-	var stream = fs.createWriteStream("data/crime2014.json");
+	var stream = fs.createWriteStream(newFile);
 	console.log(JSON.stringify( geocodedAddresses ))
 	stream.write(JSON.stringify( geocodedAddresses ));
 	stream.end();
